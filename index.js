a220282a8470c6a506f5063c8baf6fd66b0dea35
+ <lucy.njoroge1@student.moringaschool.com>
2023-06-29 23:03:43 +0300

    Assignment completed

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+// Function expression: divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function: square
+  const square = (num) => num * num;
+  
+  // Arrow function: add
+  const add = (num1, num2) => num1 + num2;
+  
